a9f545373aa954f69d267c55bb77b22e90bd6aa1
Sebastian Perez <sperez0032@gmail.com>
2023-03-22 21:33:02 -0500

    attempted to add email validation for Contact form.

---
+++ web.js
@@ -0,0 +1,30 @@
+var field = {email};
+
+document.addEventListener("DOMContentLoaded", function(){
+    field.email = document.getElementById('mail');
+});
+
+function isEmpty(value){
+    if(value == null || typeof value == 'undefined') return true;
+
+    return (value.length > 0);
+}
+
+
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+   }
+
+   function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+    field.className = 'placeholderRed';
+    } else {
+    field.className = '';
+    }
+   
+    return isFieldValid;
+   }
