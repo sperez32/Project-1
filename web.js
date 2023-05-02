6c7b1fdc123828dfd83454a94a2b8d9d5c5d0df8
Sebastian Perez <sperez0032@gmail.com>
2023-05-01 23:47:57 -0500

    Finalized Project

--- web.js
+++ web.js
@@ -1,30 +1,132 @@
-var field = {email};
+var express = require('express');
 
-document.addEventListener("DOMContentLoaded", function(){
-    field.email = document.getElementById('mail');
+var app = express();
+var mysql = require('mysql'); 
+var con = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    database : 'tapduc'
+  });
+
+  app.set("view engine", "ejs");
+  app.use(express.static(__dirname + '/views'));
+var bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: true }));
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    });
+
+    //Main Routes
+    app.get("/", function (req, res) {
+        res.render("home");
+      });
+
+      app.get("/home", function (req, res) {
+        res.render("home");
+      });
+
+      app.get("/team",function(req,res){
+        res.render("team");
+       });
+
+      
+   app.get("/interest",function(req,res){
+    res.render("interest");
+   });
+
+   
+
+ 
+   
+   app.post("/interest", function(req, res) {
+    var user_info = { firstName: req.body.name, lastName: req.body.surname,Email:req.body.email,phoneNum:req.body.phoneNum }; 
+    var q = "insert into Interest set ?";
+    con.query(q, user_info, function(error, results) {
+    if (error) throw error;
+    res.render("confirm");  
+    });
+    });
+    
+    app.get("/contact",function(req,res){
+        res.render("contact");
+       });
+    
+     
+       
+       app.post("/contact", function(req, res) {
+        var user_info = { firstName: req.body.name, lastName: req.body.surname,Email:req.body.email,phoneNum:req.body.phoneNum,Message:req.body.message}; 
+        var q = "insert into Contact set ?";
+        con.query(q, user_info, function(error, results) {
+        if (error) throw error;
+        res.render("confirm");  
+        });
+        });
+  
+
+
+
+
+      //Admin Routes 
+      app.get("/admin", function (req, res) {
+        res.render("adminHome");
+      });
+
+        app.post("/adminInsert", function(req, res) {
+          var user_info = { firstName: req.body.name, lastName: req.body.surname,Email:req.body.email,phoneNum:req.body.phoneNum }; 
+          var q = "insert into Interest set ?";
+          con.query(q, user_info, function(error, results) {
+          if (error) throw error;
+          res.redirect("/admin");
+          });
+          });
+
+          app.post("/adminUpdate", function (req, res) {
+            var oldEmail = req.body.email;
+            var q = "update Interest set Email='Need New Email' where Email=?";
+            con.query(q,oldEmail, function (error,  results) {
+              if (error) throw error;
+              res.redirect("/admin");
+            });
+          });
+
+          app.post("/adminDelete", function (req, res) {
+            var Email = req.body.email;
+            var q = "delete from Interest where Email = ?";
+            con.query(q,Email, function (error, results) {
+              if (error) throw error;
+              res.redirect("/admin");
+            });
+          });
+
+          app.post("/adminSearch", function (req, res) {
+            var lName = req.body.surname;
+            var q = "select * from Interest where lastName = ?";
+            con.query(q, lName, function (error, results) {
+              if (error) throw error;
+              res.render("search", { data: results });
+            });
+          });
+
+          app.post("/adminDisplayInterest", function (req, res) {
+            var q = "select * from Interest";
+            con.query(q, function (error, results) {
+              if (error) throw error;
+              res.render("displayInterest", { data: results });
+            });
+          });
+
+          app.post("/adminDisplayContact", function (req, res) {
+            var q = "select * from Contact";
+            con.query(q, function (error, results) {
+              if (error) throw error;
+              res.render("displayContact", { data: results });
+            });
+          });
+ 
+app.listen(8080, function () {
+ console.log('App listening on port 8080!');
 });
 
-function isEmpty(value){
-    if(value == null || typeof value == 'undefined') return true;
-
-    return (value.length > 0);
-}
-
-
-function isEmail(email) {
-    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
-   }
-
-   function fieldValidation(field, validationFunction) {
-    if (field == null) return false;
-   
-    let isFieldValid = validationFunction(field.value)
-    if (!isFieldValid) {
-    field.className = 'placeholderRed';
-    } else {
-    field.className = '';
-    }
-   
-    return isFieldValid;
-   }
+    
